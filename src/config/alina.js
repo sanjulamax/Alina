84acc5638e138dda426ea76481744206c370a858
SAVINDU ABEYWICKRAMA <sanjulasavindu@gmail.com>
2024-06-17 02:00:02 +0530

    update files#*

--- src/config/alina.js
+++ src/config/alina.js
@@ -13,7 +13,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = "AIzaSyDo1pBccWYoP9avKvAWmbFHV60NLtUggQI";
+const apiKey = import.meta.env.VITE_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
